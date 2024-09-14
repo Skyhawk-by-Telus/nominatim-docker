f2509476e38fe7da0aa3f6c37db33dfe87d004a4
Sean Hogan <sean.hogan@skyhawk.co>
2024-09-14 12:44:16 -0400

    Reworked the local.php handling for startapi.sh.
    Got rid of CR/LF pairs using a .gitattributes file.

--- 4.4/startapache.sh
+++ 4.4/startapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp /data/local.php /app/src/build/settings/local.php
+cp /data/local.php /app/local.php
 
 /usr/sbin/apache2ctl -D FOREGROUND
 tail -f /var/log/apache2/error.log
--- 4.4/startapi.sh
+++ 4.4/startapi.sh
@@ -3,16 +3,19 @@
 # Custom shim to start the nominatim Apache server based on config values from the ECS TaskDefinition
 # $DB_IP must be injected as environment variable
 
-mkdir /data
+mkdir -p /data
 
 REPLICATION_URL="http://download.geofabrik.de/north-america-updates/"
 DB_DSN="pgsql:host=$DB_IP;port=6432;user=nominatim;password=password1234;dbname=nominatim"
 
-sed \
+[ ! -f /data/local.php ] && cp /app/local-template.php /data/local.php
+
+sed -i \
     -e "s,{DB_DSN},$DB_DSN,g" \
     -e "s,{REPLICATION_URL},$REPLICATION_URL,g" \
-    < /app/src/build/settings/local.php \
-    | tee /data/local.php
+    /data/local.php \
 
-/app/startapache.sh
+cat /data/local.php
+
+bash /app/startapache.sh
 
